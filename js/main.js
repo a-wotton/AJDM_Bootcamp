c0b92ffdbef45b0591b7f80ddb9f6cc4a6d83887
a-wotton <a_wotton@fanshaweonline.ca>
2023-09-09 11:33:01 -0400

    created functionality for the hamburger menu

--- js/main.js
+++ js/main.js
@@ -51,3 +51,33 @@ document.addEventListener("DOMContentLoaded", function () {
     // Add an event listener to the button
     loadTestimonialsButton.addEventListener("click", loadTestimonials);
 });
+
+// This section allows the hamburger menu to be toggleable
+
+let menu = document.querySelector("#hammenu"),
+    listItem = document.querySelectorAll(".mobile-menu li"),
+    media = window.matchMedia("(min-width: 730px)");
+    mobileNav = document.querySelector(".mobile-menu"); 
+
+
+function toggleMenu() {
+    if (mobileNav.classList.contains("hidden")) {
+        mobileNav.classList.remove("hidden");
+    } else {
+        mobileNav.classList.add("hidden");
+    }
+}
+
+menu.addEventListener("click", toggleMenu);
+
+// This section ensures that the hamburger menu closes itself when you click a list item to scroll down the page
+
+function toggleItem() {
+    if (mobileNav.classList.contains("hidden")) {
+        mobileNav.classList.remove("hidden");
+    } else {
+        mobileNav.classList.add("hidden");
+    }
+}
+
+listItem.forEach(item => item.addEventListener("click", toggleItem));
